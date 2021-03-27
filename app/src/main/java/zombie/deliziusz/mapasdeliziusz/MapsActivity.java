f0898f8ed7bf19551078e864c71e9bea6ef878da
Deliziusz <deliziusz.666@gmail.com>
2021-03-27 02:04:57 -0600

    Okay! I think this is ready... I hope yes! Please, neeed pass this subject!!

--- app/src/main/java/zombie/deliziusz/mapasdeliziusz/MapsActivity.java
+++ app/src/main/java/zombie/deliziusz/mapasdeliziusz/MapsActivity.java
@@ -177,30 +177,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public class DirectionsJSONParser {
-
         public List<List<HashMap<String,String>>> parse(JSONObject jObject){
-
             List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String,String>>>() ;
             JSONArray jRoutes = null;
             JSONArray jLegs = null;
             JSONArray jSteps = null;
-
             try {
-
                 jRoutes = jObject.getJSONArray("routes");
-
                 for(int i=0;i<jRoutes.length();i++){
                     jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
                     List path = new ArrayList<HashMap<String, String>>();
-
                     for(int j=0;j<jLegs.length();j++){
                         jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
-
                         for(int k=0;k<jSteps.length();k++){
                             String polyline = "";
                             polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
                             List<LatLng> list = decodePoly(polyline);
-
                             for(int l=0;l<list.size();l++){
                                 HashMap<String, String> hm = new HashMap<String, String>();
                                 hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
